cad8e470b22371c03041591f189ad2a9356f349e
주현길 <wngusrlf3@naver.com>
2023-08-22 21:15:32 +0900

    Revert "8월22일 주현길 수정"
    This reverts commit 3fb1627251537ff6f768200f77f3b871ab17cda4.

--- src/main/java/com/dae/kdmes/controller/app02/App02CrudController.java
+++ src/main/java/com/dae/kdmes/controller/app02/App02CrudController.java
@@ -414,7 +414,6 @@ public class App02CrudController {
                 searchtxt = "%";
             }
             index10Dto.setPcode(searchtxt);
-            index10Dto.setJpum(searchtxt);
             index10ListDto = service10.GetFplanList(index10Dto);
 
             model.addAttribute("index10ListDto",index10ListDto);
